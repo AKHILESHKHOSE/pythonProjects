67a8368566088b8c28c4960837529092d761b8de
AKHILESHKHOSE <akhikhose3@gmail.com>
2025-06-21 14:49:12 +0530

    task1 completed

---
+++ assignment1/task1.py
@@ -0,0 +1,9 @@
+## Program to take 2 numbers as input and perform basic arithmetic operations
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
