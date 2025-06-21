6573451bbaa24b073c4224e462b12ab91b768448
AKHILESHKHOSE <akhikhose3@gmail.com>
2025-06-21 14:53:19 +0530

    assignment1 task2 completed

---
+++ assignment1/task2.py
@@ -0,0 +1,10 @@
+#Program to create personalized greeting message
+
+# taking user name as input .First name and last name
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# creating personalized greeting message
+
+print("Hello", first_name, last_name+"! Welcome to the Python programming world.")
